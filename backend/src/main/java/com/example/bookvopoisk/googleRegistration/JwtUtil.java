7990cc873e3d6902655d1f3361e3bc970b19c702
zavtema <zaviruxaartemii@gmail.com>
2025-11-03 17:32:32 +0300

    probable back

--- backend/src/main/java/com/example/bookvopoisk/googleRegistration/JwtUtil.java
+++ backend/src/main/java/com/example/bookvopoisk/googleRegistration/JwtUtil.java
@@ -10,7 +10,6 @@ import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.Date;
 import java.util.UUID;
