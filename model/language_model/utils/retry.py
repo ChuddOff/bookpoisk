d47bf1f7db3d3afec1253088e160d63fbdc981f7
Andrey Zasimov <zasimovandrey2000@gmail.com>
2025-11-04 13:37:37 +0300

    Initialization servers structure

--- model/language_model/utils/retry.py
+++
@@ -1,9 +0,0 @@
-def retry(func):
-    def wrapper(*args, **kwargs):
-        for attempt in range(5):
-            try:
-                return func(*args, **kwargs)
-            except Exception:
-                continue
-        return f"The model is not available now"
-    return wrapper
