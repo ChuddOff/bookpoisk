660b3a4f2b5c152c3e4cad2bc002794e078bcdd2
Andrey Zasimov <zasimovandrey2000@gmail.com>
2025-11-09 19:05:56 +0300

    Deploy server fix v5

--- model/server/core/config.py
+++ model/server/core/config.py
@@ -1,23 +1,13 @@
 import os
 
 from dotenv import load_dotenv
-from psycopg2.pool import SimpleConnectionPool
 
 from model.server.services import ClientManager, TaskManager, MemoryClientStore, RedisClientStore
 
 load_dotenv()
-
-DB_CONFIG = {
-    "host": os.getenv("DATABASE_HOST"),
-    "port": os.getenv("DATABASE_PORT"),
-    "dbname": os.getenv("DATABASE_NAME"),
-    "user": os.getenv("DATABASE_USER"),
-    "password": os.getenv("DATABASE_PASSWORD")
-}
 
 BACKEND_URL = os.getenv("BACKEND_URL")
 
 store = MemoryClientStore()
 client_manager = ClientManager(store)
 task_manager = TaskManager()
-pool = SimpleConnectionPool(1, 10, **DB_CONFIG)
--- model/server/database/operations.py
+++
@@ -1,9 +0,0 @@
-from model.server.models import Book
-
-
-def book_exists_in_db(book: Book) -> bool:
-    from server.database import get_cursor
-
-    with get_cursor() as cursor:
-        cursor.execute("SELECT 1 FROM books WHERE title = %s AND author = %s LIMIT 1", (book["title"], book["author"]))
-        return True if cursor.fetchone() else False
