4e2c70dce56ee2de60b265c4d906d13aabfb995f
Andrey Zasimov <zasimovandrey2000@gmail.com>
2025-11-09 19:11:44 +0300

    Deploy server fix v6

--- model/server/services/validator.py
+++ model/server/services/validator.py
@@ -1,4 +1,3 @@
-from model.server.database import book_exists_in_db
 from model.server.models import Book
 
 
@@ -6,4 +5,4 @@ def validate_answer(answer: str) -> bool:
     ...
 
 def validate_book(book: Book) -> bool:
-    return book_exists_in_db(book)
+    ...
