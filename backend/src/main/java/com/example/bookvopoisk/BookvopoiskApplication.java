45687f637f7b33f0fbf40384ba32d15a93c8c3ea
zavtema <zaviruxaartemii@gmail.com>
2025-12-01 00:30:31 +0300

    alarm

--- backend/src/main/java/com/example/bookvopoisk/BookvopoiskApplication.java
+++ backend/src/main/java/com/example/bookvopoisk/BookvopoiskApplication.java
@@ -2,11 +2,13 @@ package com.example.bookvopoisk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class BookvopoiskApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookvopoiskApplication.class, args);
 	}
-
 }
+
