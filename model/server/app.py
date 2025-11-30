caebfa40a98ce3c79a1d83765615b3eb2e496703
Andrey Zasimov <zasimovandrey2000@gmail.com>
2025-12-01 00:41:57 +0300

    Wake up task

--- model/server/app.py
+++ model/server/app.py
@@ -1,8 +1,12 @@
+import asyncio
 import os
 import sys
+import aiohttp
 
 import uvicorn
 from fastapi import FastAPI
+
+from server.core import BACKEND_URL
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
@@ -13,6 +17,22 @@ app = FastAPI(title="Bookpoisk", version="1.0")
 app.include_router(client_router)
 app.include_router(generate_router)
 app.include_router(health_router)
+
+
+async def health_endpoint():
+    while True:
+        try:
+            async with aiohttp.ClientSession() as session:
+                await session.get(BACKEND_URL)
+        except Exception:
+            pass
+
+        await asyncio.sleep(300)
+
+
+@app.on_event("startup")
+async def startup():
+    asyncio.create_task(health_endpoint())
 
 
 if __name__ == "__main__":
