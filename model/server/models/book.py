834bd624dabbb14089fd0a438e9cd03576dbbc7f
Andrey Zasimov <zasimovandrey2000@gmail.com>
2025-11-30 14:39:17 +0300

    Deploy fix v5

--- model/server/models/book.py
+++ model/server/models/book.py
@@ -1,10 +1,14 @@
-from typing import Optional
+from typing import Optional, List
 
 from pydantic import BaseModel
 
 
 class Book(BaseModel):
+    bookId: str
     title: str
     author: Optional[str] = None
     year: Optional[int] = None
     description: Optional[str] = None
+    genres: Optional[List[str]] = None
+    coverUrl: Optional[str] = None
+    pages: Optional[int] = None
